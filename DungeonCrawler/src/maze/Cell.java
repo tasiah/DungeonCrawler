c773c7ac2bca976474ec31351ba5098269d3f2d0
Tasia <tasiahalim@outlook.com>
2017-09-07 11:31:19 -0700

    oops, used opposite boolean values

--- DungeonCrawler/src/maze/Cell.java
+++ DungeonCrawler/src/maze/Cell.java
@@ -2,16 +2,16 @@ package maze;
 
 public class Cell {
 	public boolean visited;
-	public boolean north;
+	public boolean north; // is there a wall north of the cell?
 	public boolean east;
 	public boolean south;
 	public boolean west;
 	
 	public Cell() {
-		visited = true;
-		north = true;
-		east = true;
-		south = true;
-		west = true;
+		visited = false;
+		north = false;
+		east = false;
+		south = false;
+		west = false;
 	}
 }
