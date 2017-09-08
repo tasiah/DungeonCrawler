e214c6f817e7384a4b9ca4aa8b9fc8e9f7e36bfb
Tasia <tasiahalim@outlook.com>
2017-09-07 18:08:46 -0700

    added other directions

--- DungeonCrawler/src/player/Player.java
+++ DungeonCrawler/src/player/Player.java
@@ -21,5 +21,38 @@ public class Player {
 			System.out.println("moved north");
 		}
 	}
+	
+	public void moveSouth(Maze maze) {
+		if (y - 1 < 0) {
+			System.out.println("end of map");
+		} else if (maze.getCell(x, y).south) {
+			System.out.println("wall");
+		} else {
+			y--;
+			System.out.println("moved south");
+		}
+	}
+	
+	public void moveEast(Maze maze) {
+		if (x + 1 < 0) {
+			System.out.println("end of map");
+		} else if (maze.getCell(x, y).east) {
+			System.out.println("wall");
+		} else {
+			x++;
+			System.out.println("moved east");
+		}
+	}
+	
+	public void moveWest(Maze maze) {
+		if (x - 1 < 0) {
+			System.out.println("end of map");
+		} else if (maze.getCell(x, y).west) {
+			System.out.println("wall");
+		} else {
+			x--;
+			System.out.println("moved west");
+		}
+	}
 		
 }
