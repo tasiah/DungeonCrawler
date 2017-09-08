ad7122190cfd59f7b0783ead58487022e7d34a9c
Tasia <tasiahalim@outlook.com>
2017-09-07 19:55:01 -0700

    scraped and rewrote generate with randNeighbor method

--- DungeonCrawler/src/maze/Maze.java
+++ DungeonCrawler/src/maze/Maze.java
@@ -5,10 +5,12 @@ public class Maze {
 	private int n; // dimension of maze
 	private Cell[][] maze;
 	private Random r;
+	private List<String> list;
 	
 	public Maze(int dim) {
 		n = dim;
 		maze = new Cell[n][n];
+		list = new ArrayList<String>();
 		
 		// initialize maze
 		for (int i = 0; i < n; i++) {
@@ -26,38 +28,49 @@ public class Maze {
 		// if cell is in dimensions of maze
 		if (x >= 0 && x < n && y >= 0 && y < n) {
 			maze[x][y].visited = true;
+			String neighbor = randNeighbor(x, y);
 				
-			// if current cell has unvisited neighbors
-			if (!maze[x][y + 1].visited || !maze[x][y - 1].visited
-					|| !maze[x + 1][y].visited || !maze[x - 1][y].visited) { 
-				boolean chosen = false;
-				do {
-					// randomly choose an unvisited neighbor
-					int n = r.nextInt(4);
-					if (n == 0 && !maze[x][y + 1].visited) {
-						maze[x][y].north = false; // remove neighboring wall
-						maze[x][y + 1].south = false;
-						generate(x, y + 1);
-						chosen = true;
-					} else if (n == 1 && !maze[x][y - 1].visited) {
-						maze[x][y].south = false;
-						maze[x][y - 1].north = false;
-						generate(x, y - 1);
-						chosen = true;
-					} else if (n == 2 && !maze[x + 1][y].visited) {
-						maze[x][y].east = false;
-						maze[x + 1][y].west = false;
-						generate(x + 1, y);
-						chosen = true;
-					} else if (n == 3 && !maze[x - 1][y].visited) {							
-						maze[x][y].west = false;
-						maze[x - 1][y].east = false;
-						generate(x - 1, y);
-						chosen = true;
-					}
-				} while (!chosen); // ensures that an unvisited neighbor is chosen
-								
+			if (neighbor != null) {
+				if (neighbor.equals("north")) {
+					maze[x][y].north = false; // remove neighboring wall
+					maze[x][y + 1].south = false;
+					generate(x, y + 1);
+				} else if (neighbor.equals("south")) {
+					maze[x][y].south = false;
+					maze[x][y - 1].north = false;
+					generate(x, y - 1);
+				} else if (neighbor.equals("east")) {
+					maze[x][y].east = false;
+					maze[x + 1][y].west = false;
+					generate(x + 1, y);
+				} else { // neighbor.equals("west")						
+					maze[x][y].west = false;
+					maze[x - 1][y].east = false;
+					generate(x - 1, y);
+				}
 			}
+			
+		}
+	}
+	
+	public String randNeighbor(int x, int y) {
+		list.clear();
+		if (y + 1 < n && !maze[x][y + 1].visited) {
+			list.add("north");
+		}
+		if (y - 1 >= 0 && !maze[x][y - 1].visited) {
+			list.add("south");
+		}
+		if (x + 1 < n && !maze[x + 1][y].visited) {
+			list.add("east");
+		}
+		if (x - 1 >= 0 && !maze[x - 1][y].visited) {
+			list.add("west");
+		}
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			return list.get(r.nextInt(list.size()));
 		}
 	}
 	
