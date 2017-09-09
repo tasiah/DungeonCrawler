7b231b40a2f440f59a49415300c61500de8b5f4e
Tasia <tasiahalim@outlook.com>
2017-09-09 09:35:17 -0700

    added player ability to identify entrance/exit

--- DungeonCrawler/src/player/Player.java
+++ DungeonCrawler/src/player/Player.java
@@ -6,61 +6,78 @@ public class Player {
 	private int x;
 	private int y;
 	
-	public Player() {
-		x = 0;
-		y = 0;
+	public Player(Maze maze) {
+		x = maze.getEntrance().x;
+		y = maze.getEntrance().y;
 	}
 	
 	public void moveNorth(Maze maze) {
 		if (maze.getCell(x, y).north) {
 			System.out.println("wall");
-		} else if (y + 1 >= maze.getDim()) {
-			if (x >= maze.getDim() / 2) {
-				System.out.println("you did it");
-			} else {
-				System.out.println("end of map");
-			}
 		} else {
 			y++;
-			System.out.println("moved north");
+			if (atExit(maze)) {
+				System.out.println("you did it");
+			} else {
+				System.out.println("moved north");
+				if (atEntrance(maze)) {
+					System.out.println("were we here before?");
+				}
+			}
 		}
 	}
 	
 	public void moveSouth(Maze maze) {
 		if (maze.getCell(x, y).south) {
 			System.out.println("wall");
-		} else if (y - 1 < 0) {
-			System.out.println("end of map");
 		} else {
 			y--;
+			// moving south will never move the player to the exit
 			System.out.println("moved south");
+			if (atEntrance(maze)) {
+				System.out.println("were we here before?");
+			}
 		}
 	}
 	
 	public void moveEast(Maze maze) {
 		if (maze.getCell(x, y).east) {
 			System.out.println("wall");
-		} else if (x + 1 >= maze.getDim()) {
-			if (y >= maze.getDim() / 2) {
-				System.out.println("you did it");
-			} else {
-				System.out.println("end of map");
-			}
 		} else {
 			x++;
-			System.out.println("moved east");
+			if (atExit(maze)) {
+				System.out.println("you did it");
+			} else {
+				System.out.println("moved east");
+				if (atEntrance(maze)) {
+					System.out.println("were we here before?");
+				}
+			}
 		}
 	}
 	
 	public void moveWest(Maze maze) {
 		if (maze.getCell(x, y).west) {
 			System.out.println("wall");
-		} else if (x - 1 < 0) {
-			System.out.println("end of map");
 		} else {
 			x--;
-			System.out.println("moved west");
+			if (atExit(maze)) {
+				System.out.println("you did it");
+			} else {
+				System.out.println("moved west");
+				if (atEntrance(maze)) {
+					System.out.println("were we here before?");
+				}
+			}
 		}
+	}
+	
+	public boolean atExit(Maze maze) {
+		return maze.getExit().x == x && maze.getExit().y == y;
+	}
+	
+	public boolean atEntrance(Maze maze) {
+		return maze.getEntrance().x == x && maze.getEntrance().y == y;
 	}
 		
 }
