52ee9599e512bbe3eae43a7e8e6de1edebdb7903
Tasia <tasiahalim@outlook.com>
2017-09-10 11:38:28 -0700

    cleaned Monster constructor
    ...is it bad style to rely on player to initialize static maze field?
    (static because there's only 1 maze and it feels useless to initialize
    it with every creature)

--- DungeonCrawler/src/monster/Monster.java
+++ DungeonCrawler/src/monster/Monster.java
@@ -1,13 +1,10 @@
 package monster;
-import maze.Maze;
 import creature.*;
 
 public class Monster extends Creature{
 	
-	public Monster(int health, Maze maze) {
+	public Monster(int health) {
 		super("monster", health);
-		x = r.nextInt(maze.getDimX());
-		y = r.nextInt(maze.getDimY());
 		cm.addCreature(this);
 	}
 	
