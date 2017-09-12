d8b980c590cae81a627abcc713a33fccda2c15b3
Tasia <tasiahalim@outlook.com>
2017-09-12 12:06:25 -0700

    added randMove(list) -- only moves if available/valid spot

--- DungeonCrawler/src/creature/Creature.java
+++ DungeonCrawler/src/creature/Creature.java
@@ -116,6 +116,28 @@ public class Creature {
 		}
 	}
 	
+	public void randMove(List<Cell> list) {
+		list.clear();
+		if (!cell.hasNorthWall() && !maze.northCell(cell).isOccupied()) {
+			list.add(maze.northCell(cell));
+		}
+		if (!cell.hasSouthWall() && !maze.southCell(cell).isOccupied()) {
+			list.add(maze.southCell(cell));
+		}
+		if (!cell.hasEastWall() && !maze.eastCell(cell).isOccupied()) {
+			list.add(maze.eastCell(cell));
+		}
+		if (!cell.hasWestWall() && !maze.westCell(cell).isOccupied()) {
+			list.add(maze.westCell(cell));
+		}
+		if (!list.isEmpty()) {
+			Cell newCell = list.get(r.nextInt(list.size()));
+			cell.setOccupied(null);
+			cell = newCell;
+			cell.setOccupied(this);
+		}
+	}
+	
 	public void checkPlayerNearby() {
 		if (!cell.hasNorthWall() && maze.northCell(cell).isOccupied() && maze.northCell(cell).getOccupied().isPlayer()) {
 			creatureAhead = maze.northCell(cell).getOccupied();
