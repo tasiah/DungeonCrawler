b8371830c99d5a5ecd197b28217e8edcebba68f3
Tasia <tasiahalim@outlook.com>
2017-09-12 14:32:17 -0700

    added comments; edited pickRandom() to account if list.isEmpty()

--- DungeonCrawler/src/helperClasses/CellList.java
+++ DungeonCrawler/src/helperClasses/CellList.java
@@ -2,6 +2,11 @@ package helperClasses;
 import java.util.*;
 import maze.Cell;
 
+/*
+ * This class allows users to input cells into a CellList and then randomly
+ * pick one from said list. Note that the user can only pick one cell from
+ * the current CellList before it clears.
+ */
 public class CellList {
 	private static List<Cell> list;
 	private static Random r;
@@ -11,17 +16,22 @@ public class CellList {
 		r = new Random();
 	}
 	
+	// adds the given cell to list
 	public static void add(Cell c) {
 		list.add(c);
 	}
 	
+	// randomly returns a cell from list
+	// or null if list is empty;
+	// clears list after retrieving cell
 	public static Cell pickRandom() {
-		Cell cell = list.get(r.nextInt(list.size()));
-		list.clear();
-		return cell;
+		if (list.isEmpty()) {
+			return null;
+		} else {
+			Cell cell = list.get(r.nextInt(list.size()));
+			list.clear();
+			return cell;
+		}
 	}
-	
-	public static boolean isEmpty() {
-		return list.isEmpty();
-	}
+
 }
