e10a489347762a9f0b894c03be512a1f84e7a3fa
Tasia <tasiahalim@outlook.com>
2017-09-12 13:55:09 -0700

    moved randMove and hasPlayerNearby to Monster class
    since Player extends Creature, Creature shouldn't have that
    functionality

--- DungeonCrawler/src/creature/Creature.java
+++ DungeonCrawler/src/creature/Creature.java
@@ -58,41 +58,4 @@ public class Creature {
 		return creatureAhead;
 	}
 	
-	public void randMove(List<Cell> list) {
-		list.clear();
-		if (!cell.hasNorthWall() && !maze.northCell(cell).isOccupied()) {
-			list.add(maze.northCell(cell));
-		}
-		if (!cell.hasSouthWall() && !maze.southCell(cell).isOccupied()) {
-			list.add(maze.southCell(cell));
-		}
-		if (!cell.hasEastWall() && !maze.eastCell(cell).isOccupied()) {
-			list.add(maze.eastCell(cell));
-		}
-		if (!cell.hasWestWall() && !maze.westCell(cell).isOccupied()) {
-			list.add(maze.westCell(cell));
-		}
-		if (!list.isEmpty()) {
-			Cell newCell = list.get(r.nextInt(list.size()));
-			cell.setOccupied(null);
-			cell = newCell;
-			cell.setOccupied(this);
-		}
-	}
-	
-	public boolean hasPlayerNearby() {
-		if (!cell.hasNorthWall() && maze.northCell(cell).isOccupied() && maze.northCell(cell).getOccupied().isPlayer()) {
-			creatureAhead = maze.northCell(cell).getOccupied();
-		} else if (!cell.hasSouthWall() && maze.southCell(cell).isOccupied() && maze.southCell(cell).getOccupied().isPlayer()) {
-			creatureAhead = maze.southCell(cell).getOccupied();
-		} else if (!cell.hasEastWall() && maze.eastCell(cell).isOccupied() && maze.eastCell(cell).getOccupied().isPlayer()) {
-			creatureAhead = maze.eastCell(cell).getOccupied();	
-		} else if (!cell.hasWestWall() && maze.westCell(cell).isOccupied() && maze.westCell(cell).getOccupied().isPlayer()) {
-			creatureAhead = maze.westCell(cell).getOccupied();
-		} else {
-			creatureAhead = null;
-		}
-		return creatureAhead != null;
-	}
-	
 }
